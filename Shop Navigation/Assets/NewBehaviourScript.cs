acdc438c438f90e90fac24d5b895d687fcf50826
Zoe Bingham <zmb6893@g.rit.edu>
2021-02-20 13:34:43 -0800

    Prepare for coding with comments

---
+++ Shop Navigation/Assets/NewBehaviourScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    NaviShop behaviors
+    @author: NaviShopTeam
+*/
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Login
+
+        // Get the Location
+
+        // Generate the store map
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Generate the map using two sets coordinates
+    void generateMap(int[] firstCorner, int[] lastCorner)
+    {
+
+    }
+
+    // Get current Location.
+    int[] getLocation()
+    {
+
+    }
+
+    // If the current location is within the store, allow the user to search for items
+
+    // Have the user enter in the store location
+    void promptLocation()
+    {
+
+    }
+
+    // Login/Signup.
+    void promptLogin(){
+
+    }
+
+    // Ask for location access. Suggest nearest store.
+    void suggestLocations()
+    {
+
+    }
+}
