dd5746bd622a567e299a065e3d8ef9376792d689
Zoe Bingham <zmb6893@g.rit.edu>
2021-02-20 16:29:51 -0800

    ShoppingList

--- Shop Navigation/Assets/NewBehaviourScript.cs
+++ Shop Navigation/Assets/NewBehaviourScript.cs
@@ -41,7 +41,7 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Get item Location.
-    void getItemLocation(int[] sku, int[] store)
+    string getItemLocation(int[] sku, int[] store)
     {
         var client = new RestClient("https://api.wegmans.io/products/categories/561-562?Subscription-Key=50bf72311a614f5e93ec2c194104a8ff&api-version=2018-10-18");
         client.Timeout = -1;
--- Shop Navigation/Assets/ShoppingList.cs
+++ Shop Navigation/Assets/ShoppingList.cs
@@ -1,4 +1,11 @@
+/**
+Class to keep track of the Shopping list
+*/
+
+using System;
+using System.Collections.Generic;
+
 public class ShoppingList 
 {
-    
+    private List;
 }
