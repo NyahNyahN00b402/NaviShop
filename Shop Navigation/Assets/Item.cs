f5351c7aa4f3c6eb4ec07a86acd6eb0272692871
Jacob Fricano <aceofjakeskqj@gmail.com>
2021-02-20 20:16:53 -0500

    Gave Item class state, and implemented its constructor.

--- Shop Navigation/Assets/Item.cs
+++ Shop Navigation/Assets/Item.cs
@@ -1,15 +1,25 @@
-
-/**
-    Item information
-    @author: DogematicShoppers
-*/
-
 public class Item
 {
+    //Integer representing an Item's sku number
+    private int sku;
+
+    //Integer representing an Item's store number
+    private int store;
+
+    /**
+    Make a new Item
+    @param sku: the sku number of an Item
+    @param store: the store number of an Item
+    */
+    public Item(int sku, int store){
+        this.sku = sku;
+        this.store = store;
+    }
+
     // Get item Location.
-    string getItemLocation(int[] sku, int[] store)
+    string getItemLocation()
     {
-        var client = new RestClient("https://api.wegmans.io/products/categories/561-562?Subscription-Key=50bf72311a614f5e93ec2c194104a8ff&api-version=2018-10-18");
+        var client = new RestClient("https://api.wegmans.io/products/{sku}/locations/{store}?api-version=2018-10-18&Subscription-Key=50bf72311a614f5e93ec2c194104a8ff", this.sku, this.store);
         client.Timeout = -1;
         var request = new RestRequest(Method.GET);
         IRestResponse response = client.Execute(request);
