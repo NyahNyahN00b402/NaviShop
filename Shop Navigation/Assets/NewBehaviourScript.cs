3f84a37847f624f1aad36115e66f28fd9420d662
nyahnyahnoob402 <nyahnyahn00b402@gmail.com>
2021-02-20 20:55:45 -0800

    import necessary custom classes to NewBehaviorScript

--- Shop Navigation/Assets/NewBehaviourScript.cs
+++ Shop Navigation/Assets/NewBehaviourScript.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Device.Location;
+
+// Custom Classes
 using Mapping;
+using Customer;
+using Item;
+using ItemLocation;
+using ShoppingList;
+using Coupons;
 
 /**
     NaviShop behaviors
-    @author: NaviShopTeam
+    @author: PatheticShoppers
 */
 
 public class NewBehaviourScript : MonoBehaviour
@@ -26,46 +33,5 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
-    }
-
-    //Generate the map using two sets coordinates
-    void generateMap(int[] firstCorner, int[] lastCorner)
-    {
-
-    }
-
-    // Get current Location.
-    int[] getLocation()
-    {
-
-    }
-
-    // Get item Location.
-    Array[] getItemLocation(int[] sku, int[] store)
-    {
-        var client = new RestClient(string.format("https://api.wegmans.io/products/{sku}/locations/{store}?api-version=2018-10-18&Subscription-Key=50bf72311a614f5e93ec2c194104a8ff", param sku, param store);
-        client.Timeout = -1;
-        var request = new RestRequest(Method.GET);
-        IRestResponse response = client.Execute(request);
-        Console.WriteLine(response.Content);
-    }
-
-    // If the current location is within the store, allow the user to search for items
-
-    // Have the user enter in the store location
-    void promptLocation()
-    {
-
-    }
-
-    // Login/Signup.
-    void promptLogin(){
-
-    }
-
-    // Ask for location access. Suggest nearest store.
-    void suggestLocations()
-    {
-
     }
 }
