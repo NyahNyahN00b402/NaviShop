2c4fd8808aa5bd85151de9e1fb1e52d69f3667e0
Jacob Fricano <aceofjakeskqj@gmail.com>
2021-02-20 19:14:11 -0500

    Added API request code to getItemLocation method

--- Shop Navigation/Assets/NewBehaviourScript.cs
+++ Shop Navigation/Assets/NewBehaviourScript.cs
@@ -41,6 +41,14 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Get item Location.
+    void getItemLocation(int[] sku, int[] store)
+    {
+        var client = new RestClient("https://api.wegmans.io/products/categories/561-562?Subscription-Key=50bf72311a614f5e93ec2c194104a8ff&api-version=2018-10-18");
+        client.Timeout = -1;
+        var request = new RestRequest(Method.GET);
+        IRestResponse response = client.Execute(request);
+        Console.WriteLine(response.Content);
+    }
 
     // If the current location is within the store, allow the user to search for items
 
